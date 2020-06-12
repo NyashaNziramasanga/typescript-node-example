408bd0abb4f3a9c5cb322b5f61a9847dea177a3c
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-12 23:15:55 +0930

    Added JScomments

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,19 @@ const PORT = 5000;
 // Type of Application
 const app: Application = express();
 
-// Arrow function with number params and returns a number
+/**
+ * @desc   Arrow function with number params and returns a number
+ * @param  {number} num1
+ * @param  {number} num2
+ * @returns number
+ */
 const addNumbers = (num1: number, num2: number): number => num1 + num2;
 
+/**
+ * @param  {Request} req
+ * @param  {Response} res
+ * @param  {NextFunction} next
+ */
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send(`<h1>Hello from TypeScript Node 🔥 ${addNumbers(5, 5)}</h1>`);
 });
